d44d0bedaaf4361e71473b70067d10b35010ccba
Robert Bigelow <rfbigelow@mac.com>
2018-05-05 07:18:07 -0700

    Initial Commit

---
+++ RayTracer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RayTracer
+//
+//  Created by Robert Bigelow on 5/5/18.
+//  Copyright © 2018 Robert Bigelow. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
